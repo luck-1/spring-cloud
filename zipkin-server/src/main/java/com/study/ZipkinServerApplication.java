498b1304c613949f149fbde8ca90a9f26f9d7812
luck-1 <1986906062@qq.com>
2020-01-14 00:24:21 +0800

    1

--- zipkin-server/src/main/java/com/study/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/study/ZipkinServerApplication.java
@@ -3,6 +3,8 @@ package com.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import zipkin.server.internal.EnableZipkinServer;
+//import zipkin2.server.internal.EnableZipkinServer;
+//import zipkin.server.internal.EnableZipkinServer;
 
 /**
  * EurekaServer spring-cloud注册中心
